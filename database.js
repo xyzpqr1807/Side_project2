9d3e60a57a0c58050933bd377fedee6c97b913ea
xyzpqr1807 <xyzpqr1807@gmail.com>
2026-07-13 00:47:45 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -21,4 +21,6 @@ const ourVideos = [
 // Add your FULL YouTube embed codes for Music / Mixtapes here
 const ourMixtape = [
     `<iframe width="560" height="315" src="https://www.youtube.com/embed/vuVn1r0DpeE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/N8dexd1PqEw?si=e6kfDpnqE22g5TKg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+
 ];
